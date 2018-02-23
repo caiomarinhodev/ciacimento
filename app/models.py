8229c83e0440dcafe7e6225c272ea6fc4ccae693
Caio Marinho <caiomarinho8@gmail.com>
2018-02-23 09:03:17 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -30,7 +30,6 @@ class BaseAddress(models.Model):
 
 class Cliente(TimeStamped, BaseAddress):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, blank=True, null=True)
-    nome = phone = models.CharField(max_length=300, blank=True, null=True, verbose_name='Nome')
     phone = models.CharField(max_length=30, blank=True, null=True, verbose_name='Telefone')
     full_address = models.CharField(max_length=200, blank=True, null=True)
 
