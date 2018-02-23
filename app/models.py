b3641bfd31bc019e9a941c3113d7678b833623f8
Caio Marinho <caiomarinho8@gmail.com>
2018-02-23 09:26:11 -0300

    fix listagem

--- app/models.py
+++ app/models.py
@@ -48,7 +48,7 @@ class Cliente(TimeStamped, BaseAddress):
         super(Cliente, self).save(*args, **kwargs)
 
     def __str__(self):
-        return u'%s - %s - %s/PB' % (self.user.first_name, self.phone, self.cidade)
+        return u'%s - %s - %s/PB' % (self.nome, self.phone, self.cidade)
 
 
 class Vendedor(TimeStamped, BaseAddress):
