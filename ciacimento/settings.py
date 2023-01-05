94278b5ec7e38f3e5e05993978ad45eeeda86ed1
Caio Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2023-01-05 09:43:11 -0300

    add test

--- ciacimento/settings.py
+++ ciacimento/settings.py
@@ -93,11 +93,20 @@ DATABASES = {
     }
 }
 
-NAME_DB = os.getenv('DB_NAME', 'ciacimento')
+NAME_DB = os.getenv('DB_NAME', 'db.sqlite3')
 
-db_from_env = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/{}'.format(NAME_DB),
-                                     conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+if 'sqlite' in NAME_DB:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    db_from_env = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/{}'.format(NAME_DB),
+                                         conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
