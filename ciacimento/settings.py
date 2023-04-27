1dcaeec5a63abcb86073b49a78af1daf690138f7
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2023-04-26 21:41:07 -0300

    [production] test prod error

--- ciacimento/settings.py
+++ ciacimento/settings.py
@@ -29,7 +29,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
